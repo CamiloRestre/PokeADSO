b3327a8b3050289e58271c1ca976ee61e7309c68
Camilo Restrepo <161396676+CamiloRestre@users.noreply.github.com>
2025-07-17 16:52:58 -0500

    Delete ferreteria/src/main directory

--- ferreteria/src/main/java/com/ferreteria/ferreteria/model/repository/ClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ferreteria.ferreteria.model.repository;
-
-import com.ferreteria.ferreteria.model.Clientes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientesRepository extends JpaRepository<Clientes, Long>
-{
-
-}
